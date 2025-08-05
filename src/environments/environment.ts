4636bfa08d26a212618137b89448d8c45335a6b6
DesarrolloPLS <desarrollo@plasalle.mx>
2025-08-05 16:35:25 -0600

    cambios para ver si pega

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 // environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3001/api'
+  // production: false,
+  // apiUrl: 'http://localhost:3001/api',
+  production: true,
+  apiUrl: 'https://hoop-link-back-94f7ac07adc8.herokuapp.com/api'
 };
