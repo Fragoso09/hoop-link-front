aa6513db2e93e5d58624e9e88d00850981ce346e
DesarrolloPLS <desarrollo@plasalle.mx>
2025-08-05 16:37:00 -0600

    otro cvambio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 // environment.ts
 export const environment = {
-  // production: false,
-  // apiUrl: 'http://localhost:3001/api',
-  production: true,
-  apiUrl: 'https://hoop-link-back-94f7ac07adc8.herokuapp.com/api'
+  production: false,
+  apiUrl: 'http://localhost:3001/api',
 };
